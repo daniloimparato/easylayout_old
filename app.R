39dbdad3f1edb27719ff3115511ce90d12a64cc5
Danilo Imparato <imparatodanilo@gmail.com>
2019-09-25 13:55:44 -0300

    idk probably line endings

---
+++ app.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(htmltools)
+library(magrittr)
+library(igraph)
+
+setwd("C:/R/easylayout/")
+
+edgelist <- read.table(
+  "../neurotransmission/resultados/n6_edgelist.tsv"
+  ,header=T
+  ,sep="\t"
+  ,stringsAsFactors=F
+)
+gene_info <- read.table(
+  "../neurotransmission/resultados/n4_gene_info.tsv"
+  ,header=T
+  ,sep="\t"
+  ,stringsAsFactors=F
+)
+
+
+network <- graph_from_data_frame(edgelist, directed=F, vertices=gene_info)
+V(network)$size <- V(network)$system_count * 3
+
+graph_json <- jsonlite::toJSON(list(
+  nodes = igraph::as_data_frame(network, "vertices")
+  ,links = igraph::as_data_frame(network, "edges")))
+
+
+server <- function(input, output, session) {
+  session$sendCustomMessage(type = "dataTransferredFromServer", graph_json)
+  
+  observeEvent(input$mydata, {
+    if(!is.null(input$mydata)) stopApp(input$mydata)
+  })
+}
+
+addResourcePath('vivagraph.min.js', 'C:/R/easylayout/www/vivagraph.min.js')
+addResourcePath('multiselect.js', 'C:/R/easylayout/www/multiselect.js')
+addResourcePath('index.js', 'C:/R/easylayout/www/index.js')
+
+layout <- runApp(shinyApp(ui = htmlTemplate("www/index.html"), server)) %>% matrix(ncol=2,byrow=T)
+
+
+plot(network, layout = layout, vertex.size = 1, vertex.label = NA)
+
+
